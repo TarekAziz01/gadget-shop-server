2cff844a96b1ee6217ab942a4a219f3663be8138
Tarek Aziz <mdaziz3289@gmail.com>
2024-12-24 02:58:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 4000;
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+//mongodb
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bubur.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(url, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const userCollection = client.db('gadgetShop').collection('users');
+const productCollection = client.db('gadgetShop').collection('products');
+
+const dbConnect = async () => {
+    try {
+        client.connect();
+        console.log("Database connected successfully");
+
+        //insert user
+        
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await userCollection.findOne(query);
+
+            if (existingUser) {
+                return res.send({ message: "user already exists" });
+            }
+
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+
+
+    } catch (error) {
+        console.log(error.name, error.message);
+    }
+};
+
+dbConnect()
+ 
+  
+//api  
+app.get("/", (req, res) => {
+    res.send("server is running")
+});
+
+//jwt
+app.post('/authentication', async (req, res) => {
+    const userEmail = req.body
+    const token = jwt.sign(userEmail, process.env.ACCESS_KEY_TOKEN, { expiresIn: '10d' });
+    res.send( {token} );
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port,${port}`)
+}) 
